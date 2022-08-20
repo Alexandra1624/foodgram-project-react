bc6b6799febe4067152104e30b8bd8899057ef6a
Александра Радионова <sashamain@yandex.ru>
2022-08-20 14:31:41 +0300

    v1.2

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,7 +9,6 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from rest_framework import status, viewsets
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
