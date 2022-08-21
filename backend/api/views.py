9798fcfdc3fee0bc64bc798194d05422dd916ec3
Александра Радионова <sashamain@yandex.ru>
2022-08-21 13:33:27 +0300

    m5

--- backend/api/views.py
+++ backend/api/views.py
@@ -88,6 +88,5 @@ class RecipeViewSet(viewsets.ModelViewSet):
         ]
         response = HttpResponse(content, content_type="text/plain")
         response["Content-Disposition"] = (
-                'attachment; filename="shopping_list.txt"'
-        )
+                'attachment; filename="shopping_list.txt"')
         return response
