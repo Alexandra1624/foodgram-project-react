cf34dbcf87eb00d3f65fe1c562360fc41b71addc
Александра Радионова <sashamain@yandex.ru>
2022-08-21 08:12:39 +0300

    sh 9

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from users.views import CustomUserViewSet
 
+from users.views import CustomUserViewSet
+from .utils import ShoppingCardView
 from .views import IngredientViewSet, RecipeViewSet, TagViewSet
 
 app_name = 'api'
@@ -14,6 +15,11 @@ router_v1.register('recipes', RecipeViewSet, basename='recipes')
 
 
 urlpatterns = [
+    path(
+        'recipes/download_shopping_cart/',
+        ShoppingCardView.as_view(),
+        name='download_shopping_cart'
+    ),
     path('', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken'))
 ]
