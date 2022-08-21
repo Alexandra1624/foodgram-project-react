73269e936328e8d05dc9571e2c820d67c4e380fd
Александра Радионова <sashamain@yandex.ru>
2022-08-21 07:15:53 +0300

    shopping 6

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,4 @@
-import io
-
-from django.db.models import Sum
-from django.http import FileResponse
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             Shopping, Tag)
@@ -72,51 +69,37 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return post(request, pk, Shopping, RecipeFollowSerializer)
         return delete(request, pk, Shopping)
 
-    @action(
-        detail=False,
-        methods=['get'],
-        permission_classes=(IsAuthenticated,))
+    @action(detail=False, methods=['get'],
+            permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        """Качаем список с ингредиентами."""
-
-        user = request.user
-        shopping_list = RecipeIngredient.objects.filter(
-            recipe__cart__user=user).values(
-            'ingredient__name',
-            'ingredient__measurement_unit'
-        ).annotate(
-            amount_total=Sum('amount')
-        ).order_by()
-        font = 'Tantular'
+        final_list = {}
+        ingredients = RecipeIngredient.objects.filter(
+            recipe__cart__user=request.user).values_list(
+            'ingredient__name', 'ingredient__measurement_unit',
+            'amount')
+        for item in ingredients:
+            name = item[0]
+            if name not in final_list:
+                final_list[name] = {
+                    'measurement_unit': item[1],
+                    'amount': item[2]
+                }
+            else:
+                final_list[name]['amount'] += item[2]
         pdfmetrics.registerFont(
-            TTFont('Tantular', 'Tantular.ttf', 'UTF-8')
-        )
-        buffer = io.BytesIO()
-        pdf_file = canvas.Canvas(buffer)
-        pdf_file.setFont(font, 24)
-        pdf_file.drawString(
-            150,
-            800,
-            'Список покупок:'
-        )
-        pdf_file.setFont(font, 14)
-        from_bottom = 750
-        for number, ingredient in enumerate(shopping_list, start=1):
-            pdf_file.drawString(
-                50,
-                from_bottom,
-                (f'{number}.  {ingredient["ingredient__name"]} - '
-                 f'{ingredient["amount_total"]} '
-                 f'{ingredient["ingredient__measurement_unit"]}')
-            )
-            from_bottom -= 20
-            if from_bottom <= 50:
-                from_bottom = 800
-                pdf_file.showPage()
-                pdf_file.setFont(font, 14)
-        pdf_file.showPage()
-        pdf_file.save()
-        buffer.seek(0)
-        return FileResponse(
-            buffer, as_attachment=True, filename='shopping_list.pdf'
-        )
+            TTFont('Tantular', 'Tantular.ttf', 'UTF-8'))
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = ('attachment; '
+                                           'filename="shopping_list.pdf"')
+        page = canvas.Canvas(response)
+        page.setFont('Slimamif', size=24)
+        page.drawString(200, 800, 'Список ингредиентов')
+        page.setFont('Slimamif', size=16)
+        height = 750
+        for i, (name, data) in enumerate(final_list.items(), 1):
+            page.drawString(75, height, (f'<{i}> {name} - {data["amount"]}, '
+                                         f'{data["measurement_unit"]}'))
+            height -= 25
+        page.showPage()
+        page.save()
+        return response
