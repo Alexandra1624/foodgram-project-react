3e04719b6c840421e2b0b02f1cb602235657dbd7
Александра Радионова <sashamain@yandex.ru>
2022-08-21 07:42:43 +0300

    sh 7

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,8 +1,60 @@
+import io
+
+from django.db.models import Sum
+from django.http import FileResponse
 from django.shortcuts import get_object_or_404
+from recipes.models import Recipe, RecipeIngredient
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from recipes.models import Recipe
+
+class ShoppingCardView(APIView):
+    def get(self, request):
+        user = request.user
+        shopping_list = RecipeIngredient.objects.filter(
+            recipe__cart__user=user).values(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).annotate(
+            amount=Sum('amount')
+        ).order_by()
+        font = 'Tantular'
+        pdfmetrics.registerFont(
+            TTFont('Tantular', 'Tantular.ttf', 'UTF-8')
+        )
+        buffer = io.BytesIO()
+        pdf_file = canvas.Canvas(buffer)
+        pdf_file.setFont(font, 24)
+        pdf_file.drawString(
+            150,
+            800,
+            'Список покупок:'
+        )
+        pdf_file.setFont(font, 14)
+        from_bottom = 750
+        for number, ingredient in enumerate(shopping_list, start=1):
+            pdf_file.drawString(
+                50,
+                from_bottom,
+                (f'{number}.  {ingredient["ingredient__name"]} - '
+                 f'{ingredient["amount"]} '
+                 f'{ingredient["ingredient__measurement_unit"]}')
+            )
+            from_bottom -= 20
+            if from_bottom <= 50:
+                from_bottom = 800
+                pdf_file.showPage()
+                pdf_file.setFont(font, 14)
+        pdf_file.showPage()
+        pdf_file.save()
+        buffer.seek(0)
+        return FileResponse(
+            buffer, as_attachment=True, filename='shopping_list.pdf'
+        )
 
 
 def post(request, pk, model, serializer):
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,14 @@
-from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                            Shopping, Tag)
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from .filters import IngredientFilter, RecipeFilter
 from .mixins import ListRetrieveViewSet
 from .pagination import CustomPageNumberPagination
 from .permissions import IsAuthorOrReadOnly
+from recipes.models import Favorite, Ingredient, Recipe, Shopping, Tag
 from .serializers import (IngredientSerializer, RecipeFollowSerializer,
                           RecipeGetSerializer, RecipeSerializer, TagSerializer)
 from .utils import delete, post
@@ -68,38 +63,3 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if request.method == 'POST':
             return post(request, pk, Shopping, RecipeFollowSerializer)
         return delete(request, pk, Shopping)
-
-    @action(detail=False, methods=['get'],
-            permission_classes=[IsAuthenticated])
-    def download_shopping_cart(self, request):
-        final_list = {}
-        ingredients = RecipeIngredient.objects.filter(
-            recipe__cart__user=request.user).values_list(
-            'ingredient__name', 'ingredient__measurement_unit',
-            'amount')
-        for item in ingredients:
-            name = item[0]
-            if name not in final_list:
-                final_list[name] = {
-                    'measurement_unit': item[1],
-                    'amount': item[2]
-                }
-            else:
-                final_list[name]['amount'] += item[2]
-        pdfmetrics.registerFont(
-            TTFont('Tantular', 'Tantular.ttf', 'UTF-8'))
-        response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = ('attachment; '
-                                           'filename="shopping_list.pdf"')
-        page = canvas.Canvas(response)
-        page.setFont('Slimamif', size=24)
-        page.drawString(200, 800, 'Список ингредиентов')
-        page.setFont('Slimamif', size=16)
-        height = 750
-        for i, (name, data) in enumerate(final_list.items(), 1):
-            page.drawString(75, height, (f'<{i}> {name} - {data["amount"]}, '
-                                         f'{data["measurement_unit"]}'))
-            height -= 25
-        page.showPage()
-        page.save()
-        return response
