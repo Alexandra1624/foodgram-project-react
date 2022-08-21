d5fcb9f701876b168912817d1a203d90e3e0ea87
Александра Радионова <sashamain@yandex.ru>
2022-08-21 16:49:01 +0300

    shop 3

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,7 +10,6 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .filters import IngredientFilter, RecipeFilter
 from .mixins import ListRetrieveViewSet
