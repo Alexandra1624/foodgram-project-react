07dbd5234513cf3cbfbee5a51184d114f05970a4
Александра Радионова <sashamain@yandex.ru>
2022-08-21 16:31:06 +0300

    shop

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from users.views import CustomUserViewSet
-from .views import IngredientViewSet, RecipeViewSet, TagViewSet
+from .views import (IngredientViewSet, RecipeViewSet, ShoppingCardView,
+                    TagViewSet)
 
 app_name = 'api'
 
@@ -14,6 +15,11 @@ router_v1.register('recipes', RecipeViewSet, basename='recipes')
 
 
 urlpatterns = [
+    path(
+        'recipes/download_shopping_cart/',
+        ShoppingCardView.as_view(),
+        name='download_shopping_cart'
+    ),
     path('', include(router_v1.urls)),
     path('auth/', include('djoser.urls.authtoken'))
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,16 @@
+import io
+
 from django.db.models import Sum
-from django.http import HttpResponse
+from django.http import FileResponse
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .filters import IngredientFilter, RecipeFilter
 from .mixins import ListRetrieveViewSet
@@ -67,27 +73,49 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return post(request, pk, Shopping, RecipeFollowSerializer)
         return delete(request, pk, Shopping)
 
-    @action(
-        methods=["get"],
-        detail=False,
-        permission_classes=[IsAuthenticatedOrReadOnly],
-    )
-    def download_shopping_cart(self, request):
-        shopping_cart = (
-            RecipeIngredient.objects.filter(
-                recipe__cart__user=request.user
-            )
-            .values("ingredient__name", "ingredient__measurement_unit")
-            .annotate(quantity=Sum("amount"))
+
+class ShoppingCardView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request):
+        user = request.user
+        shopping_list = RecipeIngredient.objects.filter(
+            recipe__cart__user=user).values(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).annotate(
+            amount_total=Sum('amount')
+        ).order_by()
+        font = 'Tantular'
+        pdfmetrics.registerFont(
+            TTFont('Tantular', 'Tantular.ttf', 'UTF-8')
         )
-        content = [
-            f'{item["ingredient__name"]}'
-            f' {item["ingredient__measurement_unit"]}'
-            f' - {item["quantity"]}\n'
-            for item in shopping_cart
-        ]
-        response = HttpResponse(content, content_type="text/plain")
-        response[
-            "Content-Disposition"
-        ] = 'attachment; filename="shopping_list.txt"'
-        return response
+        buffer = io.BytesIO()
+        pdf_file = canvas.Canvas(buffer)
+        pdf_file.setFont(font, 24)
+        pdf_file.drawString(
+            150,
+            800,
+            'Список покупок:'
+        )
+        pdf_file.setFont(font, 14)
+        from_bottom = 750
+        for number, ingredient in enumerate(shopping_list, start=1):
+            pdf_file.drawString(
+                50,
+                from_bottom,
+                (f'{number}.  {ingredient["ingredient__name"]} - '
+                 f'{ingredient["amount_total"]} '
+                 f'{ingredient["ingredient__measurement_unit"]}')
+            )
+            from_bottom -= 20
+            if from_bottom <= 50:
+                from_bottom = 800
+                pdf_file.showPage()
+                pdf_file.setFont(font, 14)
+        pdf_file.showPage()
+        pdf_file.save()
+        buffer.seek(0)
+        return FileResponse(
+            buffer, as_attachment=True, filename='shopping_list.pdf'
+        )
